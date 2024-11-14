33a178a7e90f38bdd509f92863dd907f18a7a6e2
owenjohnsons <ojohnson@tcd.ie>
2024-11-15 10:20:32 +1100

    accelsearch for mass job runs.

--- Parkes-Pipeline/accelsearch/accelsearch-sbatchgen.sh
+++ Parkes-Pipeline/accelsearch/accelsearch-sbatchgen.sh
@@ -1,25 +1,46 @@
 path=$1
 
 for folder in $path/uwl*; do 
+    count=0
+    batch_number=1
 
-    # make directory for accelsearch
+    # Make directory for accelsearch
     mkdir -p $folder/prepdata/accelsearch
 
-    sbatch_file=$folder/prepdata/accelsearch/accelsearch.sbatch
+    # Define the sbatch file name
+    sbatch_file=$folder/prepdata/accelsearch/accelsearch_${batch_number}.sbatch
 
-    # overwrite the sbatch file
-    echo "#!/bin/bash" > $sbatch_file
-    echo "#SBATCH --job-name=accelsearch" >> $sbatch_file
-    echo "#SBATCH --output=$folder/prepdata/accelsearch/accelsearch.out" >> $sbatch_file
-    echo "#SBATCH --error=$folder/prepdata/accelsearch/accelsearch.err" >> $sbatch_file
-    echo "#SBATCH --time=24:00:00" >> $sbatch_file
-    echo "#SBATCH --mem=300GB" >> $sbatch_file
-    echo "#SBATCH --cpus-per-task=16" >> $sbatch_file
+    # Function to initialize a new sbatch file
+    initialize_sbatch_file() {
+        echo "#!/bin/bash" > $1
+        echo "#SBATCH --job-name=accelsearch" >> $1
+        echo "#SBATCH --output=$folder/prepdata/accelsearch/accelsearch_${batch_number}.out" >> $1
+        echo "#SBATCH --error=$folder/prepdata/accelsearch/accelsearch_${batch_number}.err" >> $1
+        echo "#SBATCH --time=05:00:00" >> $1
+        echo "#SBATCH --mem=2GB" >> $1  
+        echo "#SBATCH --cpus-per-task=16" >> $1
+        echo "source /fred/oz002/psrhome/scripts/psrhome.sh" >> $1
+    }
 
-    echo "source /fred/oz002/psrhome/scripts/psrhome.sh" >> $sbatch_file
-    echo "bash ~/pulsar-scripts/Parkes-Pipeline/accelsearch/accelsearch.sh $folder/prepdata" >> $sbatch_file
+  
+    initialize_sbatch_file $sbatch_file
 
+    # Loop through files and add commands
+    for file in $(find "$folder" -name "*_red.fft"); do
+        count=$((count + 1))
+        echo "bash ~/pulsar-scripts/Parkes-Pipeline/accelsearch/accelsearch-single.sh $file" >> $sbatch_file
+        
+        if [ $count -eq 10 ]; then
+            # Submit the sbatch file
+            sbatch $sbatch_file
 
-    # submit the sbatch file
-    sbatch $sbatch_file
-done 
+            # Reset count and increment batch number
+            count=0
+            batch_number=$((batch_number + 1))
+
+            # Create a new sbatch file
+            sbatch_file=$folder/prepdata/accelsearch/accelsearch_${batch_number}.sbatch
+            initialize_sbatch_file $sbatch_file
+        fi
+    done
+done
