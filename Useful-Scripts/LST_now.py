46047c54e220521a271eb6c2097e2aaac73d382d
Owen Johnson <ojohnson@tcd.ie>
2025-02-07 11:39:53 +0000

    Updating LST now script. Better handling.

--- Useful-Scripts/LST_now.py
+++ Useful-Scripts/LST_now.py
@@ -3,29 +3,48 @@
 Code Purpose:
     - To get the current Local Sidereal Time (LST) at a given location. Default Birr Castle, Ireland.
 Arguments: 
-    - Optional: Latitude, Longitude
+    - Optional: Latitude, Longitude, Time
 '''
 
-import argparse 
+import argparse
 import datetime
 from astropy.time import Time
 
-def get_LST(long):
-    # Get the current time in UTC
-    now = Time(datetime.datetime.now(), scale='utc')
+def get_LST(long, time='now'):
+    """
+    Calculate the Local Sidereal Time (LST) at a given longitude.
+
+    Args:
+    - long (float): Longitude of the location.
+    - time (str): Optional. Time in 'HH:MM' format or 'now' for current time.
+
+    Returns:
+    - lst (astropy.coordinates.Angle): The local sidereal time.
+    """
+    if time == 'now':
+        # Use the current UTC time
+        now = Time(datetime.datetime.utcnow(), scale='utc')
+    else:
+        # Parse the input time (assumes it's given in UTC)
+        try:
+            time_obj = datetime.datetime.strptime(time, "%H:%M")
+            now = Time(datetime.datetime.utcnow().replace(hour=time_obj.hour, minute=time_obj.minute, second=0), scale='utc')
+        except ValueError:
+            raise ValueError("Invalid time format. Use 'HH:MM'.")
+
     # Convert to LST
     lst = now.sidereal_time('apparent', longitude=long)
     return lst
 
 def main():
-    # Parse arguments
-    parser = argparse.ArgumentParser(description='Get the current Local Sidereal Time (LST) at a given location.')
-    parser.add_argument('--lat', type=float, default=53.2, help='Latitude of location')
-    parser.add_argument('--long', type=float, default=-7.9, help='Longitude of location')
+    parser = argparse.ArgumentParser(description='Get the current Local Sidereal Time (LST) at a given location. Default: Birr Castle, Ireland.')
+    parser.add_argument('--lat', type=float, default=53.2, help='Latitude of the location')
+    parser.add_argument('--long', type=float, default=-7.9, help='Longitude of the location')
+    parser.add_argument('--time', type=str, default='now', help="Time to get LST for in 'HH:MM' format (UTC), or 'now' for the current time.")
     args = parser.parse_args()
-    # Get LST
-    lst = get_LST(args.long)
+
+    lst = get_LST(args.long, args.time)
     print(lst)
-    
+
 if __name__ == '__main__':
     main()
