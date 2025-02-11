2eec3c4213a49610c0b29295b3b94289ff7aa48b
owenjohnsons <ojohnson@tcd.ie>
2025-02-12 03:59:27 +1100

    Swapping around cd's to appease character constraints.

--- Parkes-Pipeline/singlepulse/single_pulse_sbatch.sh
+++ Parkes-Pipeline/singlepulse/single_pulse_sbatch.sh
@@ -1,22 +1,34 @@
 path=$1
 
-for folder in $(ls -d ${path}/uwl*/prepdata/*Hz); do
+for folder in $(ls -d ${path}/uwl*/prepdata/*4.0GHz); do
 
     # echo "Processing $folder"
     
     sbatch_script="$folder/single_pulse.sbatch"
-    echo $folder
+    echo $sbatch_script
 
     echo "#!/bin/bash" > "$sbatch_script"
     echo "#SBATCH --job-name=single_pulse" >> "$sbatch_script"
     echo "#SBATCH --output=$folder/single_pulse.out" >> "$sbatch_script"
     echo "#SBATCH --error=$folder/single_pulse.err" >> "$sbatch_script"
-    echo "#SBATCH --time=01:00:00" >> "$sbatch_script"
-    echo "#SBATCH --mem=1G" >> "$sbatch_script"
+    echo "#SBATCH --time=2:00:00" >> "$sbatch_script"
+    echo "#SBATCH --mem=2G" >> "$sbatch_script"
 
     echo "source /fred/oz002/psrhome/scripts/psrhome.sh" >> "$sbatch_script"
     echo "cd $folder" >> "$sbatch_script"
-    echo "bash ~/pulsar-scripts/Parkes-Pipeline/singlepulse/single_pulse.sh ./" >> "$sbatch_script"
+    # echo "mkdir ./prepdata" >> "$sbatch_script"
+    # echo 'for file in *.inf; do [[ "$file" == *_red.inf ]] && continue; cp "$file" "${file%.inf}_red.inf"; done' >> "$sbatch_script"
+    # echo 'cp *_red.inf ./prepdata' >> "$sbatch_script"
+
+    # echo "for file in *.dat; do realfft \$file; done" >> $sbatch_script 
+    # echo "for file in *.fft; do [[ "\$file" == *_red.fft ]] || rednoise "\$file"; done" >> "$sbatch_script"
+    # echo "for file in *_red.fft; do realfft \$file; done" >> $sbatch_script
+    # echo "find . -maxdepth 1 -type f -name "*.dat" ! -name "*_red.dat" -delete" >> "$sbatch_script"
+    # echo "find . -maxdepth 1 -type f -name "*.fft" ! -name "*_red.fft" -delete" >> "$sbatch_script" 
+    # echo "single_pulse_search.py -t 0 *_red.dat" >> $sbatch_script
+    echo "for file in *_red.dat; do single_pulse_search.py \$file; done" >> $sbatch_script
+    echo "rm -r ./prepdata" >> "$sbatch_script"
+
 
     sbatch "$sbatch_script"
 done 
